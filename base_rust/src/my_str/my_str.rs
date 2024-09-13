cf69603a49231ae9aecf058b94890f74f4dbb772
class-bob <wanglongtin@163.com>
2024-09-13 09:53:19 +0800

    add: func new_str test case

--- base_rust/src/my_str/my_str.rs
+++ base_rust/src/my_str/my_str.rs
@@ -7,3 +7,20 @@ pub fn new_str() -> String {
     let s = String::from("hello");
     s
 }
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_new_str() {
+        assert_eq!(new_str(), "hello".to_string());
+        let mut s = String::new();
+        s = "hello".to_string();
+        assert_eq!(new_str(), s);
+
+        // assert_eq!(
+        //     Err(CreationError::Negative),
+        //     PositiveNonzeroInteger::new(-10)
+        // );
+        // assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+    }
+}
